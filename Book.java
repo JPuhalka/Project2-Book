8303d2588273af151f87cdaa3dc969ba4acff2d4
Jennifer Puhalka <jpuh0799@stu.raritanval.edu>
2015-09-28 18:06:29 -0400

    91

--- Book.java
+++ Book.java
@@ -20,12 +20,13 @@ class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean isCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
         refNumber = (" ");
+        courseText = isCourseText;
     }
     
     //Question 2.83
